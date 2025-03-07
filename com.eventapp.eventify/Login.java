9f5953723ab66abbf814669960650ace916be465
Chaitanyacse22 <112415164+Chaitanyacse22@users.noreply.github.com>
2025-03-07 17:00:31 +0530

    Create Login.java

---
+++ com.eventapp.eventify/Login.java
@@ -0,0 +1,80 @@
+package com.eventapp.eventity;
+
+ 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="user")
+public class Login {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String status;
+    
+	public Login() {
+		
+	}
+	
+	public Login(Long id, String name, String email, String password, String role, String status) {
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.role = role;
+	this.status = status;
+}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
