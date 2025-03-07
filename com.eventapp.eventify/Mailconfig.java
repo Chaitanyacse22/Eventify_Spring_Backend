74cb64d1bae62967ad2d309fbe42cd717ca43726
Chaitanyacse22 <112415164+Chaitanyacse22@users.noreply.github.com>
2025-03-07 17:02:43 +0530

    Create Mailconfig.java

---
+++ com.eventapp.eventify/Mailconfig.java
@@ -0,0 +1,28 @@
+package com.eventapp.eventity;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class Mailconfig {
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("swathinayak02@gmail.com");
+        mailSender.setPassword("tzylbuiptflksusa");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
